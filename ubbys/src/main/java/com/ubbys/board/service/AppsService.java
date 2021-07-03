373e0229e5f0f575964e620e1f8e9e2fffe7f4b8
sorinies <7258136+sorinies@users.noreply.github.com>
2021-07-04 06:47:48 +0900

    apps 작성 기능 (완료)
    수정.. (미완)

--- ubbys/src/main/java/com/ubbys/board/service/AppsService.java
+++ ubbys/src/main/java/com/ubbys/board/service/AppsService.java
@@ -3,12 +3,15 @@ package com.ubbys.board.service;
 import static com.ubbys.common.JDBCTemplate.*;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale.Category;
 
 import com.ubbys.board.dao.AppsDAO;
 import com.ubbys.board.vo.Apps;
 import com.ubbys.board.vo.Pagination;
+import com.ubbys.board.vo.Tag;
 
 public class AppsService extends BoardService {
 	private AppsDAO dao = new AppsDAO();
@@ -22,6 +25,11 @@ public class AppsService extends BoardService {
 	public List<Apps> selectAppsList(Pagination pagination) throws Exception {
 		Connection conn = getConnection();
 		List<Apps> appsList = dao.selectAppsList(conn, pagination);
+		for(Apps apps : appsList) {
+			String postContent = apps.getAppsSummary();
+			postContent = postContent.replaceAll("<br>", " ");
+			apps.setAppsSummary(postContent);
+		}
 		close(conn);
 		return appsList;
 	}
@@ -35,17 +43,19 @@ public class AppsService extends BoardService {
 	public Apps selectApps(int postId) throws Exception {
 		Connection conn = getConnection();
 		Apps apps = dao.selectApps(conn, postId);
+		List<Tag> tagList = dao.selectAppsTags(conn, postId);
+		apps.setTagList(tagList);
 		close(conn);
 		return apps;
 	}
 
 	/**
-	 * apps 게시물 + 태그 삽입 Service
+	 * apps 게시물 삽입 Service
 	 * @param apps
 	 * @return
 	 * @throws Exception
 	 */
-	public int insertApps(Apps apps) throws Exception {
+	public int insertApps(Apps apps, List<Tag> tagList) throws Exception {
 		Connection conn = getConnection();
 		int result = 0;
 		int postId = dao.nextPostId(conn);
@@ -56,15 +66,29 @@ public class AppsService extends BoardService {
 			postContent = postContent.replaceAll("(\r\n|\r|\n|\n\r)", "<br>");
 			apps.setPostContent(postContent);
 			apps.setPostTitle(replaceParameter(apps.getPostTitle()));
-			result = dao.insertTags(conn, apps, postId);
 			
 			result = dao.insertApps(conn, apps);
 			if(result > 0) {
-				
+				commit(conn);
+				for(Tag tag : tagList) {
+					System.out.println("postId:" + postId);
+					result = dao.insertAppsTags(conn, postId, tag.getTagId());
+					if(result == 0) {
+						rollback(conn);
+						break;
+					}
+				}
+				if(result > 0) {
+					commit(conn);
+				} else {
+					rollback(conn);
+				}
+			} else {
+				rollback(conn);
 			}
 		}
-		
-		return result;
+		close(conn);
+		return postId;
 	}
 	// XSS 방지 메소드
 	private String replaceParameter(String param) {
@@ -78,4 +102,49 @@ public class AppsService extends BoardService {
 		return result;
 	}
 
+	public List<Tag> selectTagList() throws Exception {
+		Connection conn = getConnection();
+		List<Tag> tagList = dao.selectTagList(conn);
+		return tagList;
+	}
+
+	
+	/**
+	 * apps 작성시 입력된 태그 삽입 Service
+	 * @param conn
+	 * @param tagArr
+	 * @return
+	 * @throws Exception
+	 */
+	public List<Tag> insertTagInAppsTags(String[] tagArr) throws Exception {
+		Connection conn = getConnection();
+		List<Tag> tagList = new ArrayList<Tag>();
+		ResultSet rs = null;
+		for(String tagItem : tagArr) {
+			Tag tag = new Tag();
+			int rowNo = 0;
+			int result = 0;
+			rowNo = dao.isExistTag(conn, tagItem); // DB에 등록되어 있는 태그인지 확인
+			if(rowNo > 0) { // 이미 존재하는 태그라면
+				tag.setTagId(rowNo);
+				tag.setTagName(tagItem);
+			} else { // 등록된 적 없는 태그라면
+				result = dao.insertTags(conn, tagItem);
+				if(result > 0) {
+					commit(conn);
+					rowNo = dao.isExistTag(conn, tagItem);
+					if(rowNo > 0) {
+						tag.setTagId(rowNo);
+						tag.setTagName(tagItem);
+					}
+				} else {
+					rollback(conn);
+					break;
+				}
+			}
+			tagList.add(tag);
+		}
+		close(conn);
+		return tagList;
+	}
 }
